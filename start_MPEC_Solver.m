ae9c99f41ed83409926ec0095d2b8a0a88407f31
KY-Lin22 <linkangyu1994@gmail.com>
2023-01-30 00:00:23 +0900

    script to start solver

--- start_MPEC_Solver.m
+++ start_MPEC_Solver.m
@@ -2,3 +2,5 @@
 addpath(genpath('./NIPMPEC_CasADi'))
 addpath(genpath('./Stabilized_SQP_Izmailov2015'))
 addpath(genpath('./Stabilized_SQP_Gill2017'))
+
+addpath('E:\GitHub\CasADi\casadi-windows-matlabR2016a-v3.5.5')
