213c9afc6f11c790d7471adea0a58389cdad011f
KY-Lin22 <linkangyu1994@gmail.com>
2023-03-01 21:25:04 +0900

    Update computeKKT_Matrix.m
    using the symmetric property

--- NIPMPEC_CasADi/@NIPMPEC_CasADi/computeKKT_Matrix.m
+++ NIPMPEC_CasADi/@NIPMPEC_CasADi/computeKKT_Matrix.m
@@ -13,6 +13,7 @@ d = -(Jac.PSIgSigma_diagVec - nu_G * ones(Dim.sigma, 1))./(Jac.PSIgG_diagVec - n
 nu_J_vec = -nu_J*ones(Dim.eta, 1);
 e = -(Jac.PSIphiGamma_diagVec - nu_G * ones(Dim.gamma, 1))./(Jac.PSIphiPHI_diagVec - nu_G * ones(Dim.gamma, 1));
 diagVec = [d; nu_J_vec; e];
+
 %% assemble KKT matrix
 % J = [D,                           zeros(Dim.sigma, Dim.eta),  zeros(Dim.sigma, Dim.gamma),  -G_grad;...
 %     zeros(Dim.eta, Dim.sigma),    -nu_J * eye(Dim.eta),       zeros(Dim.eta, Dim.gamma),    C_grad;...
@@ -20,9 +21,10 @@ diagVec = [d; nu_J_vec; e];
 %     -G_grad',                     C_grad',                    -PHI_grad',                   Hessian + nu_H*eye(Dim.Z)]; 
 
 if strcmp(self.Option.linearSystemSolver, 'mldivide_sparse')
+    %% sparse
     % diagVec
     i_diagVec = 1 : Dim.Node(3);
-    j_diagVec = 1 : Dim.Node(3);
+    j_diagVec = i_diagVec;
     s_diagVec = diagVec;
     % [-Gx, -Gp, -Gw]
     [i_Gx, j_Gx, s_Gx] = find(-Jac.Gx);
@@ -51,52 +53,70 @@ if strcmp(self.Option.linearSystemSolver, 'mldivide_sparse')
     [i_PHIw, j_PHIw, s_PHIw] = find(-Jac.PHIw);
     i_PHIw = i_PHIw + Dim.Node(2);
     j_PHIw = j_PHIw + Dim.Node(5);   
+       
     % [-Gx'; -Gp'; -Gw']
-    [i_GxT, j_GxT, s_GxT] = find(-Jac.Gx');
-    i_GxT = i_GxT + Dim.Node(3);
-    [i_GpT, j_GpT, s_GpT] = find(-Jac.Gp');
-    i_GpT = i_GpT + Dim.Node(4);    
-    [i_GwT, j_GwT, s_GwT] = find(-Jac.Gw');
-    i_GwT = i_GwT + Dim.Node(5);     
+    i_GxT = j_Gx;
+    j_GxT = i_Gx;
+    s_GxT = s_Gx;
+    i_GpT = j_Gp;   
+    j_GpT = i_Gp;
+    s_GpT = s_Gp; 
+    i_GwT = j_Gw;  
+    j_GwT = i_Gw;
+    s_GwT = s_Gw;
     % [Cx'; Cp'; Cw']
-    [i_CxT, j_CxT, s_CxT] = find(Jac.Cx');
-    i_CxT = i_CxT + Dim.Node(3);
-    j_CxT = j_CxT + Dim.Node(1);
-    [i_CpT, j_CpT, s_CpT] = find(Jac.Cp');
-    i_CpT = i_CpT + Dim.Node(4);
-    j_CpT = j_CpT + Dim.Node(1);
-    [i_CwT, j_CwT, s_CwT] = find(Jac.Cw');
-    i_CwT = i_CwT + Dim.Node(5);
-    j_CwT = j_CwT + Dim.Node(1);   
+    i_CxT = j_Cx;
+    j_CxT = i_Cx;
+    s_CxT = s_Cx;
+    i_CpT = j_Cp;
+    j_CpT = i_Cp;
+    s_CpT = s_Cp;
+    i_CwT = j_Cw;
+    j_CwT = i_Cw; 
+    s_CwT = s_Cw;
     % [-PHIx'; -PHIp'; -PHIw']
-    [i_PHIxT, j_PHIxT, s_PHIxT] = find(-Jac.PHIx');
-    i_PHIxT = i_PHIxT + Dim.Node(3);
-    j_PHIxT = j_PHIxT + Dim.Node(2);
-    [i_PHIpT, j_PHIpT, s_PHIpT] = find(-Jac.PHIp');
-    i_PHIpT = i_PHIpT + Dim.Node(4);
-    j_PHIpT = j_PHIpT + Dim.Node(2);    
-    [i_PHIwT, j_PHIwT, s_PHIwT] = find(-Jac.PHIw');
-    i_PHIwT = i_PHIwT + Dim.Node(5);
-    j_PHIwT = j_PHIwT + Dim.Node(2); 
+    i_PHIxT = j_PHIx;
+    j_PHIxT = i_PHIx;
+    s_PHIxT = s_PHIx;
+    i_PHIpT = j_PHIp;
+    j_PHIpT = i_PHIp;   
+    s_PHIpT = s_PHIp;
+    i_PHIwT = j_PHIw;
+    j_PHIwT = i_PHIw; 
+    s_PHIwT = s_PHIw;
+    
     % Hessian
-    [i_H, j_H, s_H] = find(Hessian + nu_H*eye(Dim.Z));
-    i_H = i_H + Dim.Node(3);
-    j_H = j_H + Dim.Node(3);
+    Hessian_diag = diag(Hessian);
+    i_H_diag = Dim.Node(3) + 1 : Dim.Node(6);
+    j_H_diag = i_H_diag;
+    s_H_diag = Hessian_diag + nu_H*ones(Dim.Z, 1);
+    
+    Hessian_tril = tril(Hessian, -1);
+    [i_H_tril, j_H_tril, s_H_tril] = find(Hessian_tril);
+    i_H_tril = i_H_tril + Dim.Node(3);
+    j_H_tril = j_H_tril + Dim.Node(3);
+    
+    i_H_tril_T = j_H_tril;
+    j_H_tril_T = i_H_tril;
+    s_H_tril_T = s_H_tril;    
+
     % J
     i = [i_diagVec';...
         i_Gx;  i_Gp;  i_Gw;  i_Cx;  i_Cp;  i_Cw;  i_PHIx;  i_PHIp;  i_PHIw;...
         i_GxT; i_GpT; i_GwT; i_CxT; i_CpT; i_CwT; i_PHIxT; i_PHIpT; i_PHIwT;...
-        i_H];
+        i_H_diag'; i_H_tril; i_H_tril_T];
     j = [j_diagVec';...
         j_Gx;  j_Gp;  j_Gw;  j_Cx;  j_Cp;  j_Cw;  j_PHIx;  j_PHIp;  j_PHIw;...
         j_GxT; j_GpT; j_GwT; j_CxT; j_CpT; j_CwT; j_PHIxT; j_PHIpT; j_PHIwT;...
-        j_H];
+        j_H_diag'; j_H_tril; j_H_tril_T];
     s = [s_diagVec;...
         s_Gx;  s_Gp;  s_Gw;  s_Cx;  s_Cp;  s_Cw;  s_PHIx;  s_PHIp;  s_PHIw;...
         s_GxT; s_GpT; s_GwT; s_CxT; s_CpT; s_CwT; s_PHIxT; s_PHIpT; s_PHIwT;...
-        s_H];
+        s_H_diag; s_H_tril; s_H_tril_T];
     J = sparse(i, j, s, Dim.Y, Dim.Y);
+
 else
+    %% dense
     % Constraint Jacobian
     ConstraintJacobian =...
         [-Jac.Gx,   -Jac.Gp,   -Jac.Gw;...
